cfd7f968374f30fc1dcb2cd57c22cd61f4426dad
seba1288 <seba128818@gmail.com>
2024-05-15 20:03:11 +0200

    2024-05-15 SW

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,43 @@
 from app import app
+from flask import render_template,request,redirect,url_for
+import requests
+from bs4 import BeautifulSoup
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
-@app.route('/')
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello(name='Seba'):
-    return f"Hello, {name}"
+@app.route('/extract', methods=['POST','GET'])
+def extract():
+    if request.method == "POST":
+        product_id = request.form.get('product_id')
+        url = f'https://www.ceneo.pl/{product_id}'
+        response = requests.get(url)
+        if response.status_code == requests.codes['ok']:
+            page_dom = BeautifulSoup(response.text, "html.parser")
+            try:
+                opinions_count = page_dom.select_one('a.product-review_link > span').text.strip()
+            except AttributeError:
+                opinions_count = 0
+            if opinions_count:
+                #extraction proces
+                return redirect(url_for('product', product_id=product_id))
+            return render_template('extract.html', error="product has no opinions ")
+        return render_template('extract.html', error="Product does not exists")
+
+
+        
+
+    return render_template('extract.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
+@app.route('/author')
+def author():
+    return render_template('author.html')
+
+@app.route('/product/<product_id>')
+def product(product_id):
+    return render_template('product.html',product_id=product_id)
